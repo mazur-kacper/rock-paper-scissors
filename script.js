47005167c9be5e293bf03c6b401bb44fb8d1c863
Kacper Mazur <109112557+mazur-kacper@users.noreply.github.com>
2023-07-18 16:28:34 +0200

    Create function that checks results of player's and computer's choice

--- script.js
+++ script.js
@@ -3,3 +3,29 @@ function getComputerChoice() {
   let randomChoice = choices[Math.floor(Math.random() * choices.length)];
   return randomChoice;
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+
+  if (playerSelection === computerSelection) {
+    return "It's a draw!";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "You lose! Paper beats Rock";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You lose! Scissors beats Paper";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "You lose! Rock beats Scissors";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You win! Rock beats Scissors";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You win! Paper beats Rock";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You win! Scissors beats Paper";
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
